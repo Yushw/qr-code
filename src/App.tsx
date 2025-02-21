d0377f6cd6d4a1cf4798472aca66569622aa00f0
akmu385 <akmu385@aucklanduni.ac.nz>
2025-02-21 22:58:23 +1300

    init comit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+
+function App() {
+
+  return (
+    <div className="flex justify-center items-center h-dvh bg-blue-100">
+      <div className="p-5 bg-white w-80 h-[499px] rounded-xl">
+         <img className="rounded-xl" src="qr-code.png" alt="" />
+
+         <div className="m-3 font-bold text-center text-xl mt-6">
+          Improve your front-end skills by building projects
+         </div>
+         <div className="m-3 mt-2 text-center text-slate-500 text-base">
+          Scan the QR code to visit Frontend Mentor and take your coding skills to the next level
+         </div>
+      </div>
+
+      
+    </div>
+  )
+}
+
+export default App
